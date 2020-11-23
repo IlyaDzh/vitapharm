7c6f78f1ef788a5a89beb0aea71bfb3f4f19f69c
Илья Долженко <ilay00@mail.ru>
2020-11-23 20:39:59 +0300

    added pop-ups

--- app/js/modal.js
+++ app/js/modal.js
@@ -1,15 +1,13 @@
 function openModal(id) {
     const modal = document.getElementById(id);
-    if (modal) {
-        document.body.parentNode.classList.add("modal-open");
-        modal.classList.add("show");
-    }
+    modal.classList.add("show");
+    document.body.parentNode.classList.add("modal-open");
 }
 
-function closeModal(id) {
+function closeModal(id, removeHtmlClass = true) {
     const modal = document.getElementById(id);
-    if (modal) {
+    modal.classList.remove("show");
+    if (removeHtmlClass) {
         document.body.parentNode.classList.remove("modal-open");
-        modal.classList.remove("show");
     }
 }
